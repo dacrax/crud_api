2afe9e820a57dc487e95b419a13d4a81747dfd17
giancarlos sanchez <giancarlos.sanchez.pereira@gmail.com>
2024-05-23 22:51:37 -0700

    final

--- django_crud_api/settings.py
+++ django_crud_api/settings.py
@@ -141,7 +141,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 #cors autoritation 
 
-CORS_ALLOWED_ORIGINS= ["http://localhost:5173"]
+#CORS_ALLOWED_ORIGINS= ["http://localhost:5173"]
 
 
 
